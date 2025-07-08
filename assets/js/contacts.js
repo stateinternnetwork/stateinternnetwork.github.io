126a735105a45e079b860763fb5e37c5c689d770
stateinternnetwork <phil.rus@protonmail.com>
2025-07-09 00:32:57 +0200

    Update contacts.js

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,4 +1,4 @@
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxC2z6VF7CjI974CtBpusnudyB_JCd4xZinLuzSsLfQFzmgY1l0_FrR1Y0a9UwSrXYC/exec'; // Replace with your actual Web App URL
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwnvD9CUrZYbK9UkiFhM41bQ720gTSBTEYRNQ7cmQO-h5t42i_jU8nrboqgXL2urtWQ/exec'; // Replace with your actual Web App URL
 
 // Initialize Leaflet map and place pins by city
 function initMap(locations) {
